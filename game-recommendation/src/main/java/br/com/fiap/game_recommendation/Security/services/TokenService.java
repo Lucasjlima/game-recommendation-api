52c1d5b2ec5c53e946f6dd0bb2e3f5e5f2368feb
Lucas <lucasjlima2006@gmail.com>
2026-03-27 00:25:56 -0300

    feat: adding token service and user repository

---
+++ game-recommendation/src/main/java/br/com/fiap/game_recommendation/Security/services/TokenService.java
@@ -0,0 +1,31 @@
+package br.com.fiap.game_recommendation.Security.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+    private final JwtEncoder encoder;
+
+
+    public String generateToken(String username) {
+        Instant now = Instant.now();
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(2, ChronoUnit.HOURS))
+                .subject(username)
+                .build();
+
+        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+}
---
+++ game-recommendation/src/main/java/br/com/fiap/game_recommendation/User/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.fiap.game_recommendation.User.repository;
+
+import br.com.fiap.game_recommendation.User.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
