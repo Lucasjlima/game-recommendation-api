272f736b1d605cb6c73d0eb4690f5e72ff7759dd
Lucas <lucasjlima2006@gmail.com>
2026-03-27 00:24:01 -0300

    feat: adding jwt annotations and configurations into main class

--- game-recommendation/src/main/java/br/com/fiap/game_recommendation/GameRecommendationApplication.java
+++ game-recommendation/src/main/java/br/com/fiap/game_recommendation/GameRecommendationApplication.java
@@ -1,9 +1,14 @@
 package br.com.fiap.game_recommendation;
 
+import br.com.fiap.game_recommendation.Security.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class GameRecommendationApplication {
 
 	public static void main(String[] args) {
